fde527c7ca18d05f4910590ba68b58df914db82b
unknown <tituskurniawan755@gmail.com>
2024-02-15 12:43:21 +0700

    penambahan gui matlab

---
+++ tampilangui.m
@@ -0,0 +1,80 @@
+function varargout = tampilangui(varargin)
+% TAMPILANGUI MATLAB code for tampilangui.fig
+%      TAMPILANGUI, by itself, creates a new TAMPILANGUI or raises the existing
+%      singleton*.
+%
+%      H = TAMPILANGUI returns the handle to a new TAMPILANGUI or the handle to
+%      the existing singleton*.
+%
+%      TAMPILANGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in TAMPILANGUI.M with the given input arguments.
+%
+%      TAMPILANGUI('Property','Value',...) creates a new TAMPILANGUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before tampilangui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to tampilangui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help tampilangui
+
+% Last Modified by GUIDE v2.5 15-Feb-2024 12:40:54
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @tampilangui_OpeningFcn, ...
+                   'gui_OutputFcn',  @tampilangui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before tampilangui is made visible.
+function tampilangui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to tampilangui (see VARARGIN)
+
+% Choose default command line output for tampilangui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes tampilangui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = tampilangui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes during object creation, after setting all properties.
+function figure1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
